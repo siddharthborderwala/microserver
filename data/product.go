dd71539a0539461c2d883eec37395fc211967ed5
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-12-15 01:26:23 +0530

    create product data

---
+++ data/product.go
@@ -0,0 +1,53 @@
+package data
+
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+type Product struct {
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+
+// a type for slice of pointer-to-product
+type Products []*Product
+
+// implement toJSON func
+func (p *Products) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+// dummy products list
+var productList = Products{
+	{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.49,
+		SKU:         "abc123",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "def456",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
+
+func GetProducts() Products {
+	return productList
+}
