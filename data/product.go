f4f86c6dd613e321845a1e2c92e337f02439c194
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-12-15 14:19:53 +0530

    add methods to add and update product
    add method to generate a random id

--- data/product.go
+++ data/product.go
@@ -2,12 +2,15 @@ package data
 
 import (
 	"encoding/json"
+	"errors"
 	"io"
+	"math"
+	"math/rand"
 	"time"
 )
 
 type Product struct {
-	ID          int     `json:"id"`
+	ID          string  `json:"id"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float32 `json:"price"`
@@ -15,6 +18,12 @@ type Product struct {
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeletedOn   string  `json:"-"`
+}
+
+// implement fromJSON func
+func (p *Product) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(p)
 }
 
 // a type for slice of pointer-to-product
@@ -26,10 +35,29 @@ func (p *Products) ToJSON(w io.Writer) error {
 	return e.Encode(p)
 }
 
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes() string {
+	b := make([]byte, 8)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func generateId() string {
+	rand.Seed(time.Now().UnixNano())
+	id := make([]byte, 8)
+	for i := 0; i < 8; i++ {
+		id[i] = byte(99 + math.Floor(rand.Float64()*26))
+	}
+	return string(id)
+}
+
 // dummy products list
 var productList = Products{
 	{
-		ID:          1,
+		ID:          "AsitUyxD",
 		Name:        "Latte",
 		Description: "Frothy milky coffee",
 		Price:       2.49,
@@ -38,7 +66,7 @@ var productList = Products{
 		UpdatedOn:   time.Now().UTC().String(),
 	},
 	{
-		ID:          2,
+		ID:          "ErrtausQ",
 		Name:        "Espresso",
 		Description: "Short and strong coffee without milk",
 		Price:       1.99,
@@ -48,6 +76,31 @@ var productList = Products{
 	},
 }
 
+var ErrorProductNotFound = errors.New("product not found")
+
+func findProduct(id string) (int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return i, nil
+		}
+	}
+	return -1, ErrorProductNotFound
+}
+
 func GetProducts() Products {
 	return productList
+}
+
+func AddProduct(p *Product) {
+	p.ID = generateId()
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id string, data *Product) error {
+	p, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	productList[p] = data
+	return nil
 }
